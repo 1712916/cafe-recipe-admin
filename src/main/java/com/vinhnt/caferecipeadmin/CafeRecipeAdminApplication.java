cf6c68901350055e68ab16067718260fcb956172
vinhnt <kieuphong2840@gmail.com>
2024-07-16 19:57:46 +0700

    create project

---
+++ src/main/java/com/vinhnt/caferecipeadmin/CafeRecipeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.vinhnt.caferecipeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeRecipeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeRecipeAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinhnt/caferecipeadmin/CafeRecipeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinhnt.caferecipeadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeRecipeAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
